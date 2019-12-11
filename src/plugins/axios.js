762e2b1357dd39622a8fd12e899b4953e2ac71b6
longdt <longdt.19@gmail.com>
2019-12-11 23:13:18 +0700

    add status code 403

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -47,7 +47,7 @@ class Services {
         this.context.$router.push('/e-500')
       }
       // authorization issue
-      else if (status === 401 || status === 404) {
+      else if (status === 401 || status === 403) {
         // TODO: handle authorization issue
         // this.context.$store.commit('User/signed_out')
         window.location.href = process.env.FRONTEND_URL + '/login'
