ddf4e392f1f8769704708ac1cdf4a76a57dd3ddf
longdt <longdt.19@gmail.com>
2020-03-22 12:04:26 +0700

    .

--- src/components/orders/table.vue
+++ src/components/orders/table.vue
@@ -141,7 +141,7 @@ import {ORDER_TYPE_LIST} from '@/constants'
 import DeleteComponent from '@/components/common/delete'
 import {ORDERS_URL} from '@/constants/endpoints'
 import UpdateComponent from './create_or_update'
-import getSummaries from '@/utils/getSummaries'
+// import getSummaries from '@/utils/getSummaries'
 
 export default {
   components: {UpdateComponent, DeleteComponent},
@@ -156,7 +156,35 @@ export default {
     }
   },
   methods: {
-    getSummaries,
+    getSummaries (params) {
+      const { columns, data } = params
+      const sums = []
+
+      if (columns.length > 0 && data.length > 0) {
+        columns.forEach((column, index) => {
+          if (column.property) {
+            const values = data.map(item => Number(item[column.property]))
+            console.log('values', values)
+            if (!values.every(value => isNaN(value))) {
+              sums[index] = values.reduce((prev, curr) => {
+                const value = Number(curr)
+                if (!isNaN(value)) {
+                  return prev + curr
+                } else {
+                  return prev
+                }
+              }, 0).toLocaleString()
+            } else {
+              sums[index] = ''
+            }
+          } else {
+            sums[index] = ''
+          }
+        })
+      }
+      sums[0] = 'Tổng'
+      return sums
+    },
     date_from_timestamp,
     done_request () {
       this.$emit('done_request')
